f48f21b849275bebaeb5cf667b0f678d60f44ff6
易召强 <yizhaoqiang@tinman.cn>
2022-11-14 18:50:46 +0800

    参数处理

--- TianyiAVManager/Classes/Recorder/TYAudioRecorder.h
+++ TianyiAVManager/Classes/Recorder/TYAudioRecorder.h
@@ -7,14 +7,14 @@
 
 #import <Foundation/Foundation.h>
 
+/// 音频录制器
+NS_ASSUME_NONNULL_BEGIN
+
 typedef enum : NSUInteger {
     TYAudioRecordType_system = 0,     // 使用系统高级api
     TYAudioRecordType_audioUnit,      // 使用audioUnit
     TYAudioRecordType_audioQueue      // 使用audioQueue
 } TYAudioRecordType;
-
-/// 音频录制器
-NS_ASSUME_NONNULL_BEGIN
 
 @interface TYAudioRecorder : NSObject
 
@@ -22,6 +22,11 @@ NS_ASSUME_NONNULL_BEGIN
 @property(nonatomic, assign) TYAudioRecordType recordType;
 /// 是否正在录音
 @property(nonatomic, assign) BOOL isRecording;
+/// 录音声道，默认双声道
+@property(nonatomic, assign) NSInteger chanle;
+/// 采样率，默认44.1kHz
+/// 位深，默认16
+
 /// 开始录音
 -(void)start;
 /// 停止录音
